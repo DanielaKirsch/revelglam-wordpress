1c7c72e2db944f55cbe2f569ca0ef4e9504d279d
Daniela Kirsch <daniela@nameface.com>
2018-02-22 15:59:28 -0500

    working example of the backbone media uploader

--- admin/js/revelglam-admin.js
+++ admin/js/revelglam-admin.js
@@ -1,77 +1,97 @@
-// VARIABLES
-// set up a single object namespace for the bigflannel thumbgrid app
-var rgadmin = {};
 
-// Variable to hold our media applicaton.
-rgadmin.mediaApplication;
+(function($) {
+	var media = wp.media;
 
-// data
+	var revelglamImagesFrame = function(parent) {
+		return {
+			createStates: function() {
+				parent.prototype.createStates.apply( this, arguments );
 
-// status
+				this.states.add([
+					new media.controller.revelglamImages({
+						id: 'revelglam-images',
+						title: 'RevelGlam',
+						
+						content: 'revelglam-images-browse',
+						
+					}),
+				]);
+			},
+		}
+	};
 
-// counters
-var h;
-var i;
-var j;
-
-// options
-
-// INITIALIZE
-
-jQuery(document).ready(function() {
-	console.log('document ready');
-	rgadmin.init();
-});
+	media.view.MediaFrame.Post = media.view.MediaFrame.Post.extend(revelglamImagesFrame(media.view.MediaFrame.Post));
 
 
-// FUNCTIONS
+	media.view.revelglamModeSelect = media.View.extend({
+		tagName:   'div',
+		className: 'revelglam-choose-mode',
+		template:  media.template('revelglam-choose-mode'),
 
-rgadmin.init = function() {
-	console.log('rgadmin.init');
- 	jQuery('.revelglam-activate').click(rgadmin.addRevelGlamClicked);
-}
+		
+	});
 
-rgadmin.addRevelGlamClicked = function(e) {
-	console.log('rgadmin.addRevelGlamClicked');
-	e.preventDefault();
-	var media = wp.media();
-	console.log(media);
+	media.controller.revelglamImages = media.controller.State.extend({
+		
+		handlers: {
+			
+			'content:create:revelglam-mode-select': 'createModeSelect',
+			
+		},
+
+		initialize: function() {
+
+			
+		},
+
+		turnBindings: function(method) {
+			
+			var frame = this.frame;
+
+			_.each(this.handlers, function(handler, event) {
+				this.frame[method](event, this[handler], this);
+			}, this);
+		},
+
+		activate: function() {
+
+			this.turnBindings('on');
+
+			this.setMode();
+		},
+
+		setMode: function() {
+			
+			this.set('content', 'revelglam-mode-select');
+		},
+
+		createModeSelect: function(content) {
+
+			content.view = new media.view.revelglamModeSelect({
+				controller: this.frame,
+				model:      this
+			});
+		},
+
+		
+	});
+
 	
-	
-	
-	// If the media frame already exists, reopen it and return.
-	if (rgadmin.mediaApplication) {
-    	rgadmin.mediaApplication.open();
-    	return;
-    }
-    
-    // Set the tabs array to empty to avoid our menu workflow from being polluted by other plugins.
-    wp.media.view.settings.tabs = [];
-	
-	
-	media.open();
-	
-	
-//	media.open();
-	
-	
-//	if (this.window === undefined) {
-//		this.window = media({
-//			title: 'Add Revel Glam image and links',
-//			library: {type: 'image'},
-//			multiple: false,
-//			button: {text: 'Insert'}
-//		});
-//		var self = this;
-//		this.window.on('select', function() {
-//			var first = self.window.state().get('selection').first().toJSON();
-//			wp.media.editor.insert('[myshortcode id="' + first.id + '"]');
-//		});
-//	}
-//	this.window.open();
-//	return false;
-	
-	
-	
-	
-}
+	$(document).ready(function() {
+		var media = wp.media;
+
+		$(document.body).on('click', '.revelglam-images-activate', function(e) {
+			e.preventDefault();
+
+			if(!media.frames.revelglam) {
+				media.frames.revelglam = wp.media.editor.open(wpActiveEditor, {
+					state: 'revelglam-images',
+					frame: 'post'
+				});
+			}
+			else {
+				media.frames.revelglam.open(wpActiveEditor);
+			}
+		});
+	});
+})(jQuery);
---
+++ revelglam-templates.php
@@ -0,0 +1,7 @@
+<script type="text/html" id="tmpl-revelglam-choose-mode">
+	<h2>Hi!</h2>
+	<p>This is the content box</p>
+</script>
+
+
+
